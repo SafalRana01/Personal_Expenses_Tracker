0b9fe37cc55520eb2b29c3fa9f54bcde64594d05
SafalRana01 <magarsafal61@gmail.com>
2025-02-02 09:24:02 +0545

    Models of category is done

---
+++ PersonalExpensesTracker/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalExpensesTracker.Models
+{
+
+    public class Category
+    {
+        public int CategoryId { get; set; }  // Unique identifier for the category
+        public string CategoryType { get; set; } // "Expense" or "Income"
+        public string CategoryName { get; set; } // Name of the category (e.g., Food, Salary)
+        public DateTime DateCreated { get; set; } // Date when the category was created
+    }
+}
